f31ef15f251f7d34f27f7f707e9f9ad1c1b1b5c6
margot03 <margot.adam@gmail.com>
2021-03-12 15:14:58 -0800

    refactor: Changing some data values

--- pv_data.py
+++ pv_data.py
@@ -4,10 +4,11 @@ k = 1.3806503 * 10 ** -23 # boltzmann constant
 Egap = 1.8 * 10 ** -19 # Bandgap of silicon (silicio cristalino)
 
 ## Data
-Iscn = 8.7 # nominal short circuit current
-Vocn = 37.7 # nominal open circuit voltage
-Imp = 8.2 # array current at MPP
-Vmp = 30.1 # array voltage at MPP
+
+Iscn = 8.21 #8.7 # nominal short circuit current
+Vocn = 32.9 #37.7 # nominal open circuit voltage
+Imp = 7.61 #8.2 # array current at MPP
+Vmp = 26.3 #30.1 # array voltage at MPP
 Pmax_e = Vmp * Imp # experimental array max power output
 Kv = -0.32/100 * Vocn # voltage temperature coefficient
 Ki = -0.032/100 * Iscn # current temperature coefficient
