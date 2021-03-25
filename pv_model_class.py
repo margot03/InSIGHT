269c6368a9fd4fa6e2a1a5fbf6c19ebf4cfc3a6f
margot03 <margot.adam@gmail.com>
2021-03-24 18:06:03 -0700

    perf: Fixed temperatures! And added demo file

--- pv_model_class.py
+++ pv_model_class.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 class PvModel:
 
-    def __init__(self, Iscn, Vocn, Imp, Vmp, Kv, Ki, Ns, Gn, Tn, Egap, err):
+    def __init__(self, Iscn, Vocn, Imp, Vmp, Kv, Ki, Ns, Gn, G, Tn, T, Egap, err):
         # Initializes all standard values
         self.Iscn = Iscn
         self.Vocn = Vocn
@@ -15,7 +15,9 @@ class PvModel:
         self.Ki = Ki
         self.Ns = Ns
         self.Gn = Gn
+        self.G = G
         self.Tn = Tn
+        self.T = T
         self.Egap = Egap
         self.err = err
         self.q = 1.602 * 10 ** -19
@@ -55,28 +57,49 @@ class PvModel:
         Vmp = self.Vmp
         Imp = self.Imp
         Tn = self.Tn
+        T = self.T
         Gn = self.Gn
+        G = self.G
         err = self.err
         Ki = self.Ki
         Kv = self.Kv
         Pmax_e = Vmp * Imp
 
-        T = Tn
-        G = Gn
         Vtn = k * Tn / q
         Vt = k * T / q
         deltaT = T - Tn
         
-        v = np.arange(0.1, Vocn, 0.1)
-        i = np.ones_like(v) * Iscn
-
+        """
+        The difference between the I0 equations is the
+        distribution of the resistors term! One distributes it
+        to only the leakage current and one distributes it to
+        both the leakage current and the deltaT term. New method
+        also extends the voltage to the open circuit voltage of
+        differing quantities, not just ending at the nominal Voc.
+        
+        original method: I0 = (Iscn*(Rs+Rp)/Rp + Ki*deltaT) * G/Gn
+        new method: I0 = (Iscn + Ki*deltaT) * (Rs+Rp)/Rp * G/Gn
+        """
         # current equations
         Ipvn = Iscn * (Rs+Rp)/Rp 
         Ipv = (Ipvn + Ki*deltaT) * G/Gn
         Isc = (Iscn + Ki*deltaT) * G/Gn
-        I0n = (Ipv - Vocn/Rp) / (math.exp(Vocn/Vt/a/Ns)-1)
-        I0 = I0n
         
+        ## Calculating the leakage current I0
+        # original method:
+        # I0n = (Ipv - Vocn/Rp) / (math.exp(Vocn/Vt/a/Ns)-1)
+        # updated I0 from original method:
+        # I0 = (((Iscn + Ki*deltaT)*(Rs+Rp)/Rp)-((Vocn + Kv*deltaT)/Rp)) / (math.exp((Vocn + Kv*deltaT)/a/Vt/Ns) - 1)
+    
+        # new method:
+        Isc_ = (  Iscn + Ki*deltaT  ) * G/Gn
+        Voc_ = (  Vocn + Kv*deltaT  )
+        Ipv_ = (Rs+Rp)/Rp * Isc_
+        I0 = (Ipv_ - Voc_/Rp)/(math.exp(Voc_/Vt/a/Ns)-1)
+
+        v = np.arange(0.1, Voc_, 0.1) #Vocn
+        i = np.ones_like(v) * Iscn
+
         # solve for the current for all voltages
         _i = i[0]
         i = self.newton_raphson(v, Ipv, I0, Rs, Rp, Vt, Ns, a, err, i)
@@ -119,14 +142,16 @@ class PvModel:
         Vmp = self.Vmp
         Imp = self.Imp
         Tn = self.Tn
+        T = self.T
         Gn = self.Gn
+        G = self.G
         err = self.err
         Ki = self.Ki
         Kv = self.Kv
         Pmax_e = Vmp * Imp
 
-        T = Tn
-        G = Gn
+        #T = Tn
+        #G = Gn
         Vtn = k * Tn / q
         Vt = k * T / q
         deltaT = T - Tn
@@ -151,9 +176,11 @@ class PvModel:
             Ipvn = Iscn * (Rs+Rp)/Rp
             Ipv = (Ipvn + Ki*deltaT) * G/Gn
             Isc = (Iscn + Ki*deltaT) * G/Gn
-            I0n = (Ipv - Vocn/Rp) / (math.exp(Vocn/Vt/a/Ns)-1)
-            I0 = I0n
-
+            #I0n = (Ipv - Vocn/Rp) / (math.exp(Vocn/Vt/a/Ns)-1)
+            #I0 = I0n
+            I0n = Iscn / (math.exp(Vocn/a/Vtn/Ns) - 1)
+            I0 = (Iscn + Ki * deltaT) / (math.exp((Vocn + Kv * deltaT)/a/Vt/Ns) - 1)
+            
             a = (Kv - Vocn/Tn) / (Ns * Vtn * (Ki/Ipvn - 3./Tn - Egap/(k*Tn**2)))
 
             Rp = Vmp * (Vmp + Imp*Rs) / (Vmp * Ipv - Vmp * I0 * math.exp((Vmp+Imp*Rs)/Vt/Ns/a)+Vmp*I0-Pmax_e)
